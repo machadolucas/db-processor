3d7e4e4d71430ee66226fbd562f4c4d7e9971d4a
esterevelyn <ester.evelyn.santos@usp.br>
2016-03-03 18:14:20 -0300

    Update Abstract.java

--- src/main/java/processor/entity/Abstract.java
+++ src/main/java/processor/entity/Abstract.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
@@ -19,7 +20,36 @@ public class Abstract {
     private String OWN;
 
     private String STAT;
+    
+    private String DA;
+    
+    private String VI;
+    
+    private String IP;
+    
+    private String DP;
+    
+    private String TI;
+    
+    private String PG;
+    
+    private String TA;
+    
+    private String JT;
+    
+    private String AID;
+    
+    private String SB;
+    
+    private String AD;
+    
+    private String PST;
+    
+    private String SO;
 
     @ManyToMany
-    private List<PT> PTs;
+    private List<PL> Pl_PL;
+    
+    @ManyToOne
+    private List<LA> LA_LA;
 }
