275aea082ef2039b824ab43495cd3847984f5568
esterevelyn <ester.evelyn.santos@usp.br>
2016-03-22 00:34:59 -0300

    Added files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Classe padr�o do SpringBoot
+ */
+@SpringBootApplication
+
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
